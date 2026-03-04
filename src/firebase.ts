dd725b2c06f782205ecb23632fc9f6ff59fe372d
zen811 <khatriharshit84@gmail.com>
2026-03-04 12:22:46 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 
 // These would normally be in .env
 const firebaseConfig = {
-  apiKey: "AIzaSyDummyKeyForDemo",
-  authDomain: "pg-buddy-demo.firebaseapp.com",
-  projectId: "pg-buddy-demo",
-  storageBucket: "pg-buddy-demo.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  apiKey: "AIzaSyAonqhOTl49zpV3X23sx8b5dPIfAVgPwgw",
+  authDomain: "pg-buddy-log.firebaseapp.com",
+  projectId: "pg-buddy-log",
+  storageBucket: "pg-buddy-log.firebasestorage.app",
+  messagingSenderId: "1023184086359",
+  appId: "1:1023184086359:web:fdd3f9432c271a8be90a12",
+  measurementId: "G-FB4E6G6S9B"
 };
 
 const app = initializeApp(firebaseConfig);
